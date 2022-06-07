c9061dd8b45a6980010647932d9c1ad91473e8c4
aksh-git <ann0m@protonmail.com>
2022-06-07 16:32:55 +0530

    design home page

--- projects/quizcafe/src/components/Home.js
+++ projects/quizcafe/src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../style/Home.css'
 
 function Home() {
   return (
     <div className='container'>
-      
+      <section className='section0'>
+        <div className='header'>
+          <span className='heading wel-txt'>WELCOME TO QUIZ|CAFE</span>
+        </div>
+      </section>
     </div>
   )
 }
