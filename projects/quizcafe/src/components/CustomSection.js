c8876f017c8a8becd57e775cde8f5df2f4d21d0a
aksh-git <ann0m@protonmail.com>
2022-06-10 23:57:44 +0530

    design quiz page

---
+++ projects/quizcafe/src/components/CustomSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../style/Csection.css'
+
+function CustomSection() {
+  return (
+    <div className='custon-section'>
+       <div className='cs-header'>
+          <div>Heading text will be here</div>
+          <div className='cs-headbtn'>
+
+          </div>
+       </div>
+    </div>
+  )
+}
+
+export default CustomSection
--- projects/quizcafe/src/components/Quiz.js
+++ projects/quizcafe/src/components/Quiz.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import '../style/Quiz.css'
 import { FiSearch } from 'react-icons/fi'
+import CustomSection from './CustomSection'
 
 function Quiz() {
   return (
     <div className="container">
       <div className="q-menu">
         <div className="header-badge">
-          <span className='txt heading'>Choose a Quiz here</span>
+          <span className='txt heading'>Explore Topics</span>
         </div> 
         <div className='search-bar'>
           <input type="search" placeholder='Find quiz by topic, domain........' />
           <div><div><FiSearch/></div></div>
         </div>
+      </div>
+      <div className='content'>
+        <CustomSection />
       </div>
     </div>
   )
