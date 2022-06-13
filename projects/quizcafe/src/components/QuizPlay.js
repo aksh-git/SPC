ac98ca9d574163821da8fdf50042d050366edb6b
aksh-git <ann0m@protonmail.com>
2022-06-13 13:15:56 +0530

    design play area

--- projects/quizcafe/src/components/QuizPlay.js
+++ projects/quizcafe/src/components/QuizPlay.js
@@ -3,10 +3,11 @@ import { useParams } from 'react-router-dom'
 import '../style/QuizPlay.css'
 
 function QuizPlay() {
+
   const { id } = useParams();
     
   return (
-    <div className='container'>
+    <div className='q-container'>
       <div className='playarea'>
         <div className='pa-1'></div>
         <div className='pa-2'></div>
