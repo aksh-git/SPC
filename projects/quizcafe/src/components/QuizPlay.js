e1f356780026e8778e762717ea72dfc3ccbceaa9
aksh-git <ann0m@protonmail.com>
2022-06-16 23:18:05 +0530

    quiz play card design

--- projects/quizcafe/src/components/QuizPlay.js
+++ projects/quizcafe/src/components/QuizPlay.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import '../style/QuizPlay.css'
+import QPCard from './QPCard';
 
 function QuizPlay() {
 
   const { id } = useParams();
-    
+  const quiz = {
+    title : "javascript fundamental",
+    decp : "demo decription goes here, which explains the quiz and its purpose.",
+    q_image : "https://www.google.com/favicon.ico",
+    published_on : new Date(),
+    published_by : "publisher name",
+    updated_on : new Date(),
+    total_plays : 99999,
+    useful_votes : 999,
+    ques_id : [ "234D4T1" , "134D4T1", "434D4T1"] //choose one by difficulty level
+  }
+  const qques = {
+    store : {
+      _id : "234D4T1",
+      type : "mcq" ,
+      allowed_time : 10,
+      diff_level : 1 // 1-10
+    },
+    ques : "This is a demo question on quizcafe?",
+    op1 : "okay",
+    op2 : "i know",
+    op3 : "good to know",
+    op4 : "just a demo",
+    cop : "op4"
+  }
+  
+
   return (
     <div className='q-container'>
-      <div className='playarea'>
-        <div className='pa-1'></div>
-        <div className='pa-2'></div>
-      </div>
+      <QPCard qques={qques} qdecp={quiz} />
     </div>
   )
 }
